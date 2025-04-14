38f4330d38d543cbcd5b3f0f35d0562d987feb67
Nouman Sharif Gul <noumansharifgul27@gmail.com>
2025-04-14 22:14:47 +0500

    working on the navbar

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require("express");
 // const mongoose = require("mongoosee");
+const path = require("path");
 
 const app = express();
 const PORT = 3000;
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
-  res.send("Welcome to the root route!");
+  // res.send("Welcome to the root route!");
+  res.render("index.ejs");
 });
 
 app.listen(PORT, () => {
