e733b72f12af9fff702b885c314a64a708addb44
Nouman Sharif Gul <noumansharifgul27@gmail.com>
2025-04-21 22:25:38 +0500

    added course route and footer

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 // const mongoose = require("mongoosee");
 const path = require("path");
+const ejsMate = require("ejs-mate");
 
 const app = express();
 const PORT = 3000;
@@ -11,11 +12,18 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 
+app.engine("ejs", ejsMate);
+
 app.get("/", (req, res) => {
   // res.send("Welcome to the root route!");
   res.render("index.ejs");
 });
 
+app.get("/courses", (req, res) => {
+  // res.send("Welcome to the course route!");
+  res.render("course.ejs");
+});
+
 app.listen(PORT, () => {
-  console.log(`App is listening at PORT; ${PORT}`);
+  console.log(`App is listening at PORT: ${PORT}`);
 });
