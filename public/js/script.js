4234ca084283888dfd4ee364a12dc6cbeffe2036
Noumansharif27 <noumansharifgul27@gmail.com>
2025-05-10 15:14:44 +0500

    added the images

--- public/js/script.js
+++ public/js/script.js
@@ -1,15 +1,15 @@
-locoScroll();
+// locoScroll();
 
-function locoScroll() {
-  scroll.on("scroll", (args) => {
-    // Get all current elements : args.currentElements
-    if (typeof args.currentElements["hey"] === "object") {
-      let progress = args.currentElements["hey"].progress;
-      console.log(progress);
-      // ouput log example: 0.34
-      // gsap example : myGsapAnimation.progress(progress);
-    }
-  });
-}
+// function locoScroll() {
+//   scroll.on("scroll", (args) => {
+//     // Get all current elements : args.currentElements
+//     if (typeof args.currentElements["hey"] === "object") {
+//       let progress = args.currentElements["hey"].progress;
+//       console.log(progress);
+//       // ouput log example: 0.34
+//       // gsap example : myGsapAnimation.progress(progress);
+//     }
+//   });
+// }
 
-console.warn("JS is working");
+console.log("JS is working");
