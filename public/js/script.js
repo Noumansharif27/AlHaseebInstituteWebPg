fbf6aac06451396dd04e0d4996e04ae79d3c6f45
Noumansharif27 <noumansharifgul27@gmail.com>
2025-05-12 17:32:40 +0500

    added the new navbar for mobile

--- public/js/script.js
+++ public/js/script.js
@@ -12,4 +12,28 @@
 //   });
 // }
 
-console.log("JS is working");
+const header = document.querySelector(".header");
+const toogleBtn = document.querySelector(".toogle-btn");
+const toogleCross = document.querySelector(".toogle-cross");
+const toogleBar = document.querySelector(".toogle-bar");
+let toogleBarCount = 0;
+
+toogleBtn.addEventListener("click", () => {
+  if (toogleBarCount == 0) {
+    toogleBar.style.right = "30%";
+    toogleCross.style.opacity = "1";
+    toogleBarCount = 1;
+  }
+});
+
+toogleCross.addEventListener("click", () => {
+  if (toogleBarCount == 1) {
+    toogleBar.style.right = "110%";
+    toogleCross.style.opacity = "0";
+    toogleBarCount = 0;
+  } else {
+    toogleBar.style.right = "30%";
+    toogleCross.style.opacity = "1";
+    toogleBarCount = 1;
+  }
+});
